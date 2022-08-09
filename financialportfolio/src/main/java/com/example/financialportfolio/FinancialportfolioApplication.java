b70a7abf74538014865ebb5de4cbaca751d3da19
asxd-10 <katreashay@gmail.com>
2022-08-09 08:19:18 +0000

    First Commit

---
+++ financialportfolio/src/main/java/com/example/financialportfolio/FinancialportfolioApplication.java
@@ -0,0 +1,13 @@
+package com.example.financialportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialportfolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinancialportfolioApplication.class, args);
+    }
+
+}
---
+++ financialportfolio/src/test/java/com/example/financialportfolio/FinancialportfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.financialportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialportfolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
